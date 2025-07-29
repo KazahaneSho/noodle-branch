6434573f6009c7249326a4c27dc9e75eb895511b
daoudsaiai <daoudsaiai@live.com>
2025-07-29 13:55:29 +0900

    Setup Source and add FeaturePolicy for Game Features

---
+++ Source/NoodleBranch/Public/NoodlingFeaturePolicy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFeaturesProjectPolicies.h"
+#include "NoodlingFeaturePolicy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOODLEBRANCH_API UNoodlingFeaturePolicy : public UDefaultGameFeaturesProjectPolicies
+{
+	GENERATED_BODY()
+	
+};
