4074ae23fcc733cd3af0a184cbde968580b33cd8
daoudsaiai <daoudsaiai@live.com>
2025-07-29 14:19:40 +0900

    Add ModularGameplayActors from Lyra

--- Source/NoodleBranch/NoodleBranch.Build.cs
+++ Source/NoodleBranch/NoodleBranch.Build.cs
@@ -8,7 +8,7 @@ public class NoodleBranch : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameFeatures", "ModularGameplay", "ModularGameplayActors" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
