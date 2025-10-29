1df5a3b7951abbeb0469979ddadcdb527f877a82
daoudsaiai <daoudsaiai@live.com>
2025-10-29 10:45:24 +0900

    Implemented NoodlingDeveloperSettings.cpp

--- Source/NoodleBranch/Private/Development/NoodlingDeveloperSettings.cpp
+++ Source/NoodleBranch/Private/Development/NoodlingDeveloperSettings.cpp
@@ -3,6 +3,59 @@
 
 #include "Development/NoodlingDeveloperSettings.h"
 
+#include "Framework/Notifications/NotificationManager.h"
+#include "Widgets/Notifications/SNotificationList.h"
+
+#define LOCTEXT_NAMESPACE "NoodlingCheats"
+
 UNoodlingDeveloperSettings::UNoodlingDeveloperSettings()
 {
 }
+
+FName UNoodlingDeveloperSettings::GetCategoryName() const
+{
+	return FApp::GetProjectName();
+}
+
+#if WITH_EDITOR
+void UNoodlingDeveloperSettings::PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	ApplySettings();
+}
+
+void UNoodlingDeveloperSettings::PostReloadConfig(FProperty* PropertyThatWasLoaded)
+{
+	Super::PostReloadConfig(PropertyThatWasLoaded);
+
+	ApplySettings();
+}
+
+void UNoodlingDeveloperSettings::PostInitProperties()
+{
+	Super::PostInitProperties();
+
+	ApplySettings();
+}
+
+void UNoodlingDeveloperSettings::OnPlayInEditorStarted() const
+{
+	// Show a notification toast to remind the user that there's an experience override set
+	if (ExperienceOverride.IsValid())
+	{
+		FNotificationInfo Info(FText::Format(
+			LOCTEXT("ExperienceOverrideActive", "Developer Settings Override\nExperience {0}"),
+			FText::FromName(ExperienceOverride.PrimaryAssetName)
+		));
+		Info.ExpireDuration = 2.0f;
+		FSlateNotificationManager::Get().AddNotification(Info);
+	}
+}
+
+void UNoodlingDeveloperSettings::ApplySettings()
+{
+	// This is a Stub
+}
+#endif // WITH_EDITOR
+
--- Source/NoodleBranch/Public/Development/NoodlingDeveloperSettings.h
+++ Source/NoodleBranch/Public/Development/NoodlingDeveloperSettings.h
@@ -2,7 +2,6 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Engine/DeveloperSettingsBackedByCVars.h"
 #include "NoodlingDeveloperSettings.generated.h"
 
@@ -42,13 +41,71 @@ public:
 
 	UNoodlingDeveloperSettings();
 
+	//~UDeveloperSettings interface
+	virtual FName GetCategoryName() const override;
+	//~End of UDeveloperSettings interface
+
+	//~UObject interface
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
+	virtual void PostReloadConfig(FProperty* PropertyThatWasLoaded) override;
+	virtual void PostInitProperties() override;
+#endif // WITH_EDITOR
+	//~End of UObject interface
+
 public:
 
 	// The experience override to use for Play in Editor (if not set, the default for the world settings of the open map will be used)
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, config, Category=NoodleBranch, meta=(AllowedTypes="NoodlingExperienceDefinition"))
 	FPrimaryAssetId ExperienceOverride;
 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, config, Category=NoodleBranchBots, meta=(InlineEditConditionToggle))
+	bool bOverrideBotCount = false;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, config, Category=NoodleBranchBots, meta=(EditCondition=bOverrideBotCount))
+	int32 OverrideNumPlayerBotsToSpawn = 0;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, config, Category=NoodleBranchBots)
+	bool bAllowPlayerBotsToAttack = true;
+
+	// Do the full game flow when playing in the editor, or skip 'waiting for player' / etc... game phases?
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, config, Category=NoodleBranch)
+	bool bTestFullGameFlowInPIE = false;
+
+	/**
+	* Should force feedback effects be played, even if the last input device was not a gamepad?
+	* The default behavior in NoodleBranch is to only play force feedback if the most recent input device was a gamepad.
+	*/
+	UPROPERTY(config, EditAnywhere, Category = NoodleBranch, meta = (ConsoleVariable = "NoodleBranchPlayerController.ShouldAlwaysPlayForceFeedback"))
+	bool bShouldAlwaysPlayForceFeedback = false;
+
+	// Should game logic load cosmetic backgrounds in the editor or skip them for iteration speed?
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, config, Category=NoodleBranch)
+	bool bSkipLoadingCosmeticBackgroundsInPIE = false;
+
 	// List of cheats to auto-run during 'play in editor'
 	UPROPERTY(config, EditAnywhere, Category=NoodleBranch)
 	TArray<FNoodlingCheatToRun> CheatsToRun;
+
+	// Should messages broadcast through the gameplay message subsystem be logged?
+	UPROPERTY(config, EditAnywhere, Category=GameplayMessages, meta=(ConsoleVariable="GameplayMessageSubsystem.LogMessages"))
+	bool LogGameplayMessages = false;
+
+#if WITH_EDITORONLY_DATA
+	/** A list of common maps that will be accessible via the editor detoolbar */
+	UPROPERTY(config, EditAnywhere, BlueprintReadOnly, Category=Maps, meta=(AllowedClasses="/Script/Engine.World"))
+	TArray<FSoftObjectPath> CommonEditorMaps;
+#endif // WITH_EDITORONLY_DATA
+
+#if WITH_EDITOR
+public:
+	// Called by the editor engine to let us pop reminder notifications when cheats are active
+	NOODLEBRANCH_API void OnPlayInEditorStarted() const;
+
+private:
+	// Use this to tie updates into the UObject interface Property/Config changes
+	void ApplySettings();
+#endif // WITH_EDITOR
+
+	
 };
